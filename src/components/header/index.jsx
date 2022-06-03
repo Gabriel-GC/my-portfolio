f0325c04ca7f6c5b6c43d6e368b1b8e99e6c3c90
Gabriel Gonçalves Cunha <goncalves.cunhaa@gmail.com>
2022-06-03 17:28:24 -0300

    rel=noreferrer

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -8,16 +8,16 @@ export const Header = () => {
     return (
         <div className="container-header">
             <div className="content-header">
-                <a href="https://api.whatsapp.com/send?phone=5547999963122&text=Ol%C3%A1%2C%20vamos%20marcar%20uma%20call%20%3F" target="_blank" rel="noopener" >Contato</a>
+                <a href="https://api.whatsapp.com/send?phone=5547999963122&text=Ol%C3%A1%2C%20vamos%20marcar%20uma%20call%20%3F" target="_blank" rel="noreferrer" >Contato</a>
             </div>
             <div className="content-header">
-                <a href="https://www.instagram.com/gabriel.goncalvesc/?hl=pt-br" target="_blank" rel="noopener" >
+                <a href="https://www.instagram.com/gabriel.goncalvesc/?hl=pt-br" target="_blank" rel="noreferrer" >
                     <img src={Instagram} alt="Logo Instagram" />
                 </a>
-                <a href="https://www.facebook.com/gabriel.goncalves.9797" target="_blank" rel="noopener" >
+                <a href="https://www.facebook.com/gabriel.goncalves.9797" target="_blank" rel="noreferrer" >
                     <img src={Facebook} alt="Logo Facebook" />
                 </a>
-                <a href="https://www.linkedin.com/in/gabriel-gon%C3%A7alves-cunha-42a67214a/" target="_blank" rel="noopener" >
+                <a href="https://www.linkedin.com/in/gabriel-gon%C3%A7alves-cunha-42a67214a/" target="_blank" rel="noreferrer" >
                     <img src={Linkedin} alt="Logo Linkedin" />
                 </a>
             </div>
