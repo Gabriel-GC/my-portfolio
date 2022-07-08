e818fad477e73eb6ea774db790e53bee400a3ef3
Gabriel Gonçalves Cunha <goncalves.cunhaa@gmail.com>
2022-07-08 10:34:42 -0300

    v.0009 - autoselect language

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,6 +1,6 @@
 import './styles.css';
 
-
+import React, { useEffect } from "react";
 
 import { Presentation } from '../../components/presentation';
 import { Skills } from '../../components/skills';
@@ -11,8 +11,34 @@ import { Contact } from '../../components/contact';
 import { Navbar } from '../../components/navbar';
 import { SetLanguage } from '../../components/setLanguage';
 
+import { i18n } from "../../translate/i18n";
+const I18N_STORAGE_KEY = "i18nextLng";
+
+
 
 export const Home = () => {
+
+  let mobWidth = 789;
+
+  const handleSelectChange = (value) => {
+    console.log(value);
+    localStorage.setItem(I18N_STORAGE_KEY, value);
+    document.location.reload(true)
+  };
+
+  useEffect(() => {
+    console.log(i18n.language);
+    if (i18n.language === "pt-BR") {
+      console.log("caiu");
+      handleSelectChange("pt");
+    }
+    if (window.outerWidth < mobWidth) {
+      if (i18n.language !== "en") {
+        handleSelectChange("en");
+      }
+    }
+  });
+
   return (
     <>
       <Navbar />
