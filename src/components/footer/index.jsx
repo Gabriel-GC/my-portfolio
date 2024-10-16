30f4160e2b7c2eef7d31f003395ec57d061e6764
GabrielGC <goncalves.cunhaa@gmail.com>
2024-10-16 09:17:49 -0300

    inserted logo on footer and css

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,9 +1,12 @@
 import './styles.css';
+import Logo from "../../assets/photos/favicon-64X64.png"
+
 
 export const Footer = () => {
     return (
         <div className="container-footer">
             <div className="content-footer">
+                <img src={Logo} alt="" />
                 <p className="copy">@ site developed by Gabriel Gonçalves Cunha · 2024</p>
                 <p className="copy">All honor and glory given to God</p>
             </div>
